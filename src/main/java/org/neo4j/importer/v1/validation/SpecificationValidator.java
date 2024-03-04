5786af7b4cb160590c0b1ddfde9cd5124c569cc7
Florent Biville <florent.biville@neo4j.com>
2024-03-04 12:29:25 +0100

    Mention validation order in SPI docs

--- src/main/java/org/neo4j/importer/v1/validation/SpecificationValidator.java
+++ src/main/java/org/neo4j/importer/v1/validation/SpecificationValidator.java
@@ -42,12 +42,15 @@ import org.neo4j.importer.v1.validation.SpecificationValidationResult.Builder;
  * errors are reported via {@link SpecificationValidationResult.Builder#addError(String, String, String)} and warnings
  * via {@link SpecificationValidationResult.Builder#addWarning(String, String, String)}.
  * Implementations are not expected to be thread-safe.
- * Modifying the provided arguments via any of the visitXxx or accept calls is considered undefined behavior.
+ * Implementations must not make any assumptions about the invocation order of other implementations.
+ * If an implementation depends on the successful validation of another one, please include the latter to {@link SpecificationValidator#requires()}.
+ * Mutating the provided arguments via any of the visitXxx or accept calls is considered undefined behavior.
  */
 public interface SpecificationValidator extends Comparable<SpecificationValidator> {
 
     /**
      * Reports validation errors and warnings via {@link SpecificationValidationResult.Builder}
+     *
      * @return true if at least 1 error was reported, false otherwise
      */
     boolean report(SpecificationValidationResult.Builder builder);
