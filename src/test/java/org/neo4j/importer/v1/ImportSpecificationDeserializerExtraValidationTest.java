6d3be831424a055bf22b585529344461f35637c2
Florent Biville <445792+fbiville@users.noreply.github.com>
2024-06-12 12:40:07 +0200

    feat: add coverage for node point indexes (#80)

--- src/test/java/org/neo4j/importer/v1/ImportSpecificationDeserializerExtraValidationTest.java
+++ src/test/java/org/neo4j/importer/v1/ImportSpecificationDeserializerExtraValidationTest.java
@@ -2091,4 +2091,78 @@ public class ImportSpecificationDeserializerExtraValidationTest {
                         "0 warning(s)",
                         "$.targets.nodes[0].schema.text_indexes[0].property \"invalid\" is not part of the property mappings");
     }
+
+    @Test
+    public void fails_if_node_target_point_index_refers_to_non_existent_label() {
+        assertThatThrownBy(() -> deserialize(new StringReader(
+                        """
+        {
+          "version": "1",
+          "sources": [{
+            "name": "a-source",
+            "type": "jdbc",
+            "data_source": "a-data-source",
+            "sql": "SELECT id, name FROM my.table"
+          }],
+          "targets": {
+            "nodes": [{
+              "name": "a-node-target",
+              "source": "a-source",
+              "labels": ["Label"],
+              "properties": [
+                {"source_field": "id", "target_property": "id"}
+              ],
+              "schema": {
+                "point_indexes": [
+                    {"name": "a point index", "label": "Invalid", "property": "id"}
+                ]
+              }
+            }]
+          }
+        }
+        """
+                                .stripIndent())))
+                .isInstanceOf(InvalidSpecificationException.class)
+                .hasMessageContainingAll(
+                        "1 error(s)",
+                        "0 warning(s)",
+                        "$.targets.nodes[0].schema.point_indexes[0].label \"Invalid\" is not part of the defined labels");
+    }
+
+    @Test
+    public void fails_if_node_target_point_index_property_refers_to_non_existent_property() {
+        assertThatThrownBy(() -> deserialize(new StringReader(
+                        """
+        {
+          "version": "1",
+          "sources": [{
+            "name": "a-source",
+            "type": "jdbc",
+            "data_source": "a-data-source",
+            "sql": "SELECT id, name FROM my.table"
+          }],
+          "targets": {
+            "nodes": [{
+              "name": "a-node-target",
+              "source": "a-source",
+              "labels": ["Label"],
+              "properties": [
+                {"source_field": "id", "target_property": "id"}
+              ],
+              "schema": {
+                "point_indexes": [
+                    {"name": "a point index", "label": "Label", "property": "invalid"}
+                ]
+              }
+            }]
+          }
+        }
+        """
+                                .stripIndent())))
+                .isInstanceOf(InvalidSpecificationException.class)
+                .hasMessageContainingAll(
+                        "1 error(s)",
+                        "0 warning(s)",
+                        "$.targets.nodes[0].schema.point_indexes[0].property \"invalid\" is not part of the property mappings");
+    }
 }
