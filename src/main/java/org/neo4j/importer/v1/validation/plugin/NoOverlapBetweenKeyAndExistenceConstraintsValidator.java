3da064641bfb9667425f1fcd0cc946fbd7f66e9d
Florent Biville <florent.biville@neo4j.com>
2024-08-08 17:32:08 +0200

    feat: forbid redundancy between node key and existence constraints

---
+++ src/main/java/org/neo4j/importer/v1/validation/plugin/NoOverlapBetweenKeyAndExistenceConstraintsValidator.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.importer.v1.validation.plugin;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.neo4j.importer.v1.targets.NodeTarget;
+import org.neo4j.importer.v1.validation.SpecificationValidationResult.Builder;
+import org.neo4j.importer.v1.validation.SpecificationValidator;
+
+public class NoOverlapBetweenKeyAndExistenceConstraintsValidator implements SpecificationValidator {
+
+    private static final String ERROR_CODE = "NOVL-001";
+
+    private final Map<String, List<List<String>>> invalidPaths;
+
+    public NoOverlapBetweenKeyAndExistenceConstraintsValidator() {
+        this.invalidPaths = new LinkedHashMap<>();
+    }
+
+    @Override
+    public Set<Class<? extends SpecificationValidator>> requires() {
+        return Set.of(
+                NoDanglingLabelInExistenceConstraintValidator.class,
+                NoDanglingLabelInKeyConstraintValidator.class,
+                NoDanglingPropertyInExistenceConstraintValidator.class,
+                NoDanglingPropertyInKeyConstraintValidator.class);
+    }
+
+    @Override
+    public void visitNodeTarget(int index, NodeTarget target) {
+        var schema = target.getSchema();
+        if (schema == null) {
+            return;
+        }
+        var paths = new LinkedHashMap<LabelAndProperty, List<String>>();
+        var existenceBasePath = String.format("$.targets.nodes[%d].schema.existence_constraints", index);
+        var existenceConstraints = schema.getExistenceConstraints();
+        for (int i = 0; i < existenceConstraints.size(); i++) {
+            var constraint = existenceConstraints.get(i);
+            var labelAndProp = new LabelAndProperty(constraint.getLabel(), constraint.getProperty());
+            paths.computeIfAbsent(labelAndProp, (key) -> new ArrayList<>(1))
+                    .add(String.format("%s[%d]", existenceBasePath, i));
+        }
+        var keyBasePath = String.format("$.targets.nodes[%d].schema.key_constraints", index);
+        var keyConstraints = schema.getKeyConstraints();
+        for (int i = 0; i < keyConstraints.size(); i++) {
+            var constraint = keyConstraints.get(i);
+            for (String property : constraint.getProperties()) {
+                var labelAndProp = new LabelAndProperty(constraint.getLabel(), property);
+                paths.computeIfAbsent(labelAndProp, (key) -> new ArrayList<>(1))
+                        .add(String.format("%s[%d]", keyBasePath, i));
+            }
+        }
+        List<List<String>> redundancies =
+                paths.values().stream().filter(strings -> strings.size() > 1).collect(Collectors.toList());
+
+        var schemaPath = String.format("$.targets.nodes[%d].schema", index);
+        invalidPaths.put(schemaPath, redundancies);
+    }
+
+    @Override
+    public boolean report(Builder builder) {
+        invalidPaths.forEach((schemaPath, redundancies) -> redundancies.forEach((redundantDefinitions) -> {
+            String redundantDefs = redundantDefinitions.stream()
+                    .map(def -> def.replace(schemaPath + ".", ""))
+                    .collect(Collectors.joining(", "));
+            builder.addError(
+                    schemaPath,
+                    ERROR_CODE,
+                    String.format(
+                            "%s defines overlapping key and existence constraint definitions: %s",
+                            schemaPath, redundantDefs));
+        }));
+        return !invalidPaths.isEmpty();
+    }
+}
+
+final class LabelAndProperty {
+
+    private final String label;
+    private final String property;
+
+    public LabelAndProperty(String label, String property) {
+        this.label = label;
+        this.property = property;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LabelAndProperty)) return false;
+        LabelAndProperty that = (LabelAndProperty) o;
+        return Objects.equals(label, that.label) && Objects.equals(property, that.property);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label, property);
+    }
+}
