18dd044df16a842b97f1197ed2a8493979516445
Florent Biville <florent.biville@neo4j.com>
2025-11-11 10:42:22 +0000

    refactor: do not keep order for source names

--- core/src/main/java/org/neo4j/importer/v1/validation/plugin/NoDanglingSourceValidator.java
+++ core/src/main/java/org/neo4j/importer/v1/validation/plugin/NoDanglingSourceValidator.java
@@ -16,8 +16,8 @@
  */
 package org.neo4j.importer.v1.validation.plugin;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -37,7 +37,7 @@ public class NoDanglingSourceValidator implements SpecificationValidator {
     private final Map<String, String> pathToSourceName;
 
     public NoDanglingSourceValidator() {
-        sourceNames = new LinkedHashSet<>();
+        sourceNames = new HashSet<>();
         pathToSourceName = new LinkedHashMap<>();
     }
 
