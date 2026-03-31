03d2f03f64f1c5d3e88beead7d907f729eb96fa0
GregHib <greghib@users.noreply.github.com>
2026-03-31 09:10:38 +0100

    Formatting

--- src/commonMain/kotlin/validate/node/NodeConstraints.kt
+++ src/commonMain/kotlin/validate/node/NodeConstraints.kt
@@ -1,11 +1,27 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package validate.node
 
 import model.GraphModel
 import model.Node
 import model.constraint.NodeConstraint
 import validate.Issue
-import validate.node.NodeValidation
 import validate.Validation
+import validate.node.NodeValidation
 import kotlin.collections.iterator
 
 object NodeConstraints : NodeValidation {
@@ -22,8 +38,8 @@ object NodeConstraints : NodeValidation {
                 issues.add(
                     Issue(
                         code = "missing_node_constraint_property",
-                        message = "Missing property with id '${property}' for node constraint '$constraintId'",
-                        path = "nodes.$nodeId.constraints.$constraintId.properties.${property}"
+                        message = "Missing property with id '$property' for node constraint '$constraintId'",
+                        path = "nodes.$nodeId.constraints.$constraintId.properties.$property"
                     )
                 )
             }
@@ -34,8 +50,8 @@ object NodeConstraints : NodeValidation {
                 issues.add(
                     Issue(
                         code = "missing_node_constraint_label",
-                        message = "Missing label with id '${label}' for node constraint '$constraintId'",
-                        path = "nodes.$nodeId.constraints.$constraintId.labels.${label}"
+                        message = "Missing label with id '$label' for node constraint '$constraintId'",
+                        path = "nodes.$nodeId.constraints.$constraintId.labels.$label"
                     )
                 )
             }
--- src/commonMain/kotlin/validate/node/NodeIndexesExists.kt
+++ src/commonMain/kotlin/validate/node/NodeIndexesExists.kt
@@ -1,11 +1,27 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package validate.node
 
 import model.GraphModel
 import model.Node
 import model.index.NodeIndex
 import validate.Issue
-import validate.node.NodeValidation
 import validate.Validation
+import validate.node.NodeValidation
 import kotlin.collections.iterator
 
 object NodeIndexesExists : NodeValidation {
@@ -24,8 +40,8 @@ object NodeIndexesExists : NodeValidation {
             issues.add(
                 Issue(
                     code = "missing_node_index_property",
-                    message = "Missing property with id '${property}' for node index '$indexId'",
-                    path = "nodes.$nodeId.indexes.$indexId.properties.${property}"
+                    message = "Missing property with id '$property' for node index '$indexId'",
+                    path = "nodes.$nodeId.indexes.$indexId.properties.$property"
                 )
             )
         }
