94afa4ecd4cff8b85fbffdacd0edac6aafb98d79
GregHib <greghib@users.noreply.github.com>
2026-04-13 12:43:46 +0100

    Add js plain object mapping tests

---
+++ src/jsTest/kotlin/model/LabelsJsTest.kt
@@ -0,0 +1,29 @@
+package model
+
+import model.extension.StringValue
+import model.extension.stringValueJs
+import kotlin.test.assertEquals
+
+class LabelsJsTest : JsMappingTest<Labels, LabelsJs>() {
+
+    override fun createClass() = Labels(
+        identifier = "test-id",
+        implied = setOf("a", "b"),
+        optional = setOf("c"),
+        extensions = mutableMapOf(
+            "key1" to StringValue("val1")
+        )
+    )
+
+    override fun toJs(k: Labels): LabelsJs = k.toJs()
+
+    override fun toClass(js: LabelsJs): Labels = js.toClass()
+
+    override fun verifyJsObject(jsObject: LabelsJs) {
+        assertEquals("test-id", jsObject.identifier)
+        assertEquals(2, jsObject.implied.size)
+        assertEquals("a", jsObject.implied[0])
+        assertJsEquals(stringValueJs("val1"), jsObject.extensions["key1"])
+    }
+
+}
