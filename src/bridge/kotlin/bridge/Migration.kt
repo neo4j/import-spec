b21f54a839346e3b78c5a87adffd668809c5880e
Ollie Knowles <ollie.knowles@neo4j.com>
2026-04-29 10:40:07 +0100

    condense migration bridge methods down to a single migrate method

--- src/bridge/kotlin/bridge/Migration.kt
+++ src/bridge/kotlin/bridge/Migration.kt
@@ -22,27 +22,24 @@ import codec.schema.SchemaMap
 import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.serialization.json.Json
 import migrate.MigrationPath
-import model.Type
-import model.Version
 import kotlin.experimental.ExperimentalNativeApi
 
+private val format = JsonFormat(Json {
+    ignoreUnknownKeys = true
+    isLenient = true
+    prettyPrint = false
+})
+
 @OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
-@CName("migrate_to_graph_spec")
-fun migrateToGraphSpec(inputJson: CPointer<ByteVar>?, inputType: CPointer<ByteVar>?, outputBuffer: CPointer<ByteVar>?, bufferSize: Int) =
-    invokeBridge(inputJson, inputType, outputBuffer = outputBuffer, bufferSize = bufferSize) { input ->
+@CName("migrate")
+fun migrate(inputJson: CPointer<ByteVar>?, inputType: CPointer<ByteVar>?, targetType: CPointer<ByteVar>?, targetVersion: CPointer<ByteVar>?, outputBuffer: CPointer<ByteVar>?, bufferSize: Int) =
+    invokeBridge(inputJson, inputType, targetType, targetVersion, outputBuffer = outputBuffer, bufferSize = bufferSize) { input ->
         val path = MigrationPath(GraphSpec.Json.configuration.migrations)
-        val format = JsonFormat.default
         val schema = format.decodeFromString(input[0])
         var map = schema as? SchemaMap ?: error("Schema format expected")
-        map = path.migrate(map, input[1], Version.LATEST, Type.GRAPH_SPEC)
+        map = path.migrate(map, type = input[1], targetType = input[2], targetVersion = input[3])
         format.encodeToString(map)
     }
 
-@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
-@CName("migrate_from_graph_spec")
-fun migrateFromGraphSpec(inputJson: CPointer<ByteVar>?, targetType: CPointer<ByteVar>?, targetVersion: CPointer<ByteVar>?, outputBuffer: CPointer<ByteVar>?, bufferSize: Int) =
-    invokeBridge(inputJson, targetType, targetVersion, outputBuffer = outputBuffer, bufferSize = bufferSize) { input ->
-        val graphModel = GraphSpec.Json.decodeFromString(content = input[0])
-        GraphSpec.Json.encodeToString(graphModel, targetType = input[1], targetVersion = input[2])
-    }
