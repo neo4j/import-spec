a7caacef6e131a727478e9a6b30b16806a6dbb29
Ali Ince <ali.ince@neo4j.com>
2026-05-06 12:24:54 +0100

    style: format kotlin scripts

--- .teamcity/builds/NightlyBuild.kt
+++ .teamcity/builds/NightlyBuild.kt
@@ -6,44 +6,42 @@ import jetbrains.buildServer.configs.kotlin.toId
 import jetbrains.buildServer.configs.kotlin.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-class NightlyBuild(name: String): Project({
-    this.id(name.toId())
-    this.name = name
+class NightlyBuild(name: String) :
+    Project({
+      this.id(name.toId())
+      this.name = name
 
-    val complete = Empty("${name}-complete", "complete")
+      val complete = Empty("${name}-complete", "complete")
 
-    val bts = sequential {
+      val bts = sequential {
         dependentBuildType(SemgrepCheck("${name}-semgrep-check", "semgrep check"))
         dependentBuildType(complete)
-    }
+      }
 
-    bts.buildTypes().forEach {
+      bts.buildTypes().forEach {
         it.thisVcs()
 
-        it.features {
-            enableCommitStatusPublisher()
-        }
+        it.features { enableCommitStatusPublisher() }
 
         buildType(it)
-    }
+      }
 
-    complete.triggers {
+      complete.triggers {
         vcs { enabled = false }
 
         schedule {
-            branchFilter = buildString {
-                appendLine("+:main")
-                appendLine("+:refs/heads/main")
-            }
-            schedulingPolicy = daily {
-                hour = 7
-                minute = 0
-            }
-            triggerBuild = always()
-            withPendingChangesOnly = false
-            enforceCleanCheckout = true
-            enforceCleanCheckoutForDependencies = true
+          branchFilter = buildString {
+            appendLine("+:main")
+            appendLine("+:refs/heads/main")
+          }
+          schedulingPolicy = daily {
+            hour = 7
+            minute = 0
+          }
+          triggerBuild = always()
+          withPendingChangesOnly = false
+          enforceCleanCheckout = true
+          enforceCleanCheckoutForDependencies = true
         }
-    }
-
-})
+      }
+    })
