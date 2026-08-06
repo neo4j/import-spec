60e57fedfcda87bd6b4d5582da35bacf27ff115d
Florent Biville <florent.biville@neo4j.com>
2026-08-06 16:57:13 +0200

    feat: restrict node references to a single key/unique constraint
    The existing validators allows key mappings to refer to more than
    key or unique constraint of the corresponding node target.
    
    This is not allowed anymore.
    
    BREAKING CHANGE: node reference key mappings cannot refer to
    multiple key/unique constraints anymore

--- core/src/test/java/org/neo4j/importer/v1/ImportSpecificationDeserializerTest.java
+++ core/src/test/java/org/neo4j/importer/v1/ImportSpecificationDeserializerTest.java
@@ -1795,7 +1795,54 @@ class ImportSpecificationDeserializerTest {
 
     @ParameterizedTest
     @EnumSource(SpecFormat.class)
+    void fails_if_relationship_start_node_reference_matches_no_exact_node_key_or_unique_constraint(
+            SpecFormat format, TestInfo testInfo) {
+
+        assertThatThrownBy(() -> {
+                    try (var reader = specReader(format, testInfo)) {
+                        deserialize(reader);
+                    }
+                })
+                .isInstanceOf(InvalidSpecificationException.class)
+                .hasMessageContainingAll(
+                        "1 error(s)",
+                        "0 warning(s)",
+                        "[EONR-001][$.targets.relationships[0].start_node_reference.key_mappings] Invalid key mapping for node reference 'a-node-target'. Expected it to exactly match 1 node target key or unique constraint, but it exactly matches 0 node target key or unique constraint.");
+    }
+
+    @ParameterizedTest
+    @EnumSource(SpecFormat.class)
+    void fails_if_relationship_start_node_reference_exactly_matches_several_node_key_or_unique_constraints(
+            SpecFormat format, TestInfo testInfo) {
+
+        assertThatThrownBy(() -> {
+                    try (var reader = specReader(format, testInfo)) {
+                        deserialize(reader);
+                    }
+                })
+                .isInstanceOf(InvalidSpecificationException.class)
+                .hasMessageContainingAll(
+                        "1 error(s)",
+                        "0 warning(s)",
+                        "[EONR-001][$.targets.relationships[0].start_node_reference.key_mappings] Invalid key mapping for node reference 'a-node-target'. Expected it to exactly match 1 node target key or unique constraint, but it exactly matches 2 node target key or unique constraints:\n\t$.targets.nodes[0].schema.key_constraints[0]\n\t$.targets.nodes[0].schema.key_constraints[1]");
+    }
+
+    @ParameterizedTest
+    @EnumSource(SpecFormat.class)
     void does_not_fail_if_relationship_start_node_reference_refers_to_node_key_property(
+            SpecFormat format, TestInfo testInfo) {
+
+        assertThatCode(() -> {
+                    try (var reader = specReader(format, testInfo)) {
+                        deserialize(reader);
+                    }
+                })
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @EnumSource(SpecFormat.class)
+    void does_not_fail_if_relationship_node_references_have_no_key_mapping_when_several_constraints_exist(
             SpecFormat format, TestInfo testInfo) {
 
         assertThatCode(() -> {
